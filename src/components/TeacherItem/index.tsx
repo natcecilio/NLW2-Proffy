d428ebf03a236e43d87d86f23c634ebe62c735ce
unknown <tatafly2009@hotmail.com>
2020-08-03 18:42:43 -0300

    first commit

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars0.githubusercontent.com/u/8084944?s=460&u=e24c6e38292e0e4fe7f2cffddfff7e7c4b6beefc&v=4" alt="Nathalia" />
+        <div>
+          <strong>TataFly</strong>
+          <span>Química</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /><br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vidas as pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
